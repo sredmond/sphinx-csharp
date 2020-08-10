0b9cd4d4b04303d326d75ead467d9ca7fef5c95b
Sam Redmond <sredmond@stanford.edu>
2020-08-10 14:22:29 -0700

    Migrates package documentation to docs/ folder and includes basic Sphinx tests.
    For now, the package will stay MIT-licensed, since it's still being adapted from
    `djungelorm/csharp_domain`, rather than the BSD 2-clause license suggested by
    the cookiecutter template.

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+"""
+    pytest config for sphinxcontrib/csharpdomain/tests
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: Copyright 2020 by Sam Redmond <author_email>
+    :license: MIT, see LICENSE for details.
+"""
+
+pytest_plugins = 'sphinx.testing.fixtures'
