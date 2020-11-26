7a8a9910db298261852943b07601711296c7f69b
Sam Redmond <contact+github@sredmond.io>
2020-11-25 17:08:00 -0800

    Rewrite the documentation's configuration and example usage.
    The previous sample documentation in `index.rst` has been moved to
    `csharpdomain-example.rst`, and updated to include the additional C# language
    support (e.g. indexers). The actual `index.rst` is currently just a table of
    contents, but could be expanded.
    
    The `conf.py` configuration file has been adapted to use the newest options of
    the latest `sphinx` - although the `nitpick_includes` mostly carried over.
    
    Most frustratingly, none of the usual `sys.path` modifications enabled Python to
    find the package `sphinxcontrib.csharpdomain`, because the `sphinxcontrib`
    namespace package is already in `sys.modules` at the beginning of `conf.py`, due
    to the `.pth` configuration files in `site-packages` that Python runs on startup
    (unless started with `-S`).
    
    Therefore, if sphinxcontrib.csharpdomain is importable (from a PyPI-based
    `pip install` or even a local `pip install --editable
    path/to/sphinxcontrib-csharpdomain`), everything will be fine. However, if not,
    it's necessary to somehow tell `sphinxcontrib` to reevaluate where it looks for
    subpackages. The best solution I could come up with is commented out at the head
    of the file, but it's hacky at best, and it feels like there must be a better
    way. I found [1] extremely useful in understanding namespace packages, so I'm
    saving it here for future reference.
    
    The new `Makefile` and `make.bat` come directly from `sphinx-quickstart`'s
    generated code.
    
    [1]: https://packaging.python.org/guides/packaging-namespace-packages/

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,59 @@
-project = 'C# Examples'
-version = ''
-release = '0.1'
-copyright = '2015-2016, djungelorm'
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a few common options.
+# For the full list, see: https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-master_doc = 'index'
-source_suffix = '.rst'
-extensions = ['sphinx_csharp.csharp']
+# -- Path setup --------------------------------------------------------------
+# Nothing needed, as long as `sphinxcontrib.csharpdomain` is importable.
+# If it isn't importable, then you'll need to manually point Python at the
+# namespace package. From what I can see, this isn't cleanly supported, so you'd
+# need to uncomment the following (hacky) lines or find another solution:
+#
+#     import importlib, pathlib
+#
+#     import sphinxcontrib
+#     sphinxcontrib = importlib.reload(sphinxcontrib)
+#     sphinxcontrib.__path__.append(str(pathlib.Path('.').resolve().parent / 'sphinxcontrib'))
+#
+# Hopefully, I can rewrite this snippet to use sys.path instead of the namespace
+# package's own `__path__` attribute, but no reasonable attempt has worked.
 
-pygments_style = 'sphinx'
 
+# -- Project information -----------------------------------------------------
+
+project = 'sphinxcontrib-csharpdomain'
+copyright = '2020, Sam Redmond'
+author = 'Sam Redmond'
+
+# The full version, including alpha/beta/rc tags.
+release = '0.0.1'
+
+
+# -- General configuration ---------------------------------------------------
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# For this documentation, we'll only need to use the csharpdomain subpackage.
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-extensions
+extensions = [
+    'sphinxcontrib.csharpdomain'
+]
+
+# Ignored file patterns when looking for source files.
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-exclude_patterns
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# When in nitpicky mode, tell Sphinx to ignore a few specific missing targets.
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-nitpick_ignore
 nitpick_ignore = [
     ('csharp:type', 'void'),
     ('csharp:type', 'T'),
-    ('csharp:type', 'List')
 ]
+
+
+# -- Options for HTML output -------------------------------------------------
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# The "theme" for HTML output.
+# See: https://www.sphinx-doc.org/en/master/usage/theming.html
+html_theme = 'pyramid'
